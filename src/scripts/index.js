b3b3cda48a33311edd99f9c41332c13dc30d21cc
mary.uraeva <mary.uraeva@productmadness.com>
2022-11-15 08:00:42 +0100

    initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,35 @@
+// Получить элементы
+const popupOpenButton = document.querySelector(".menu__open-popup");
+const popup = document.querySelector(".popup");
+const popupCloseButton = popup.querySelector(".popup__close");
+
+const popupToggle = function (event) {
+  popup.classList.toggle("popup_opened");
+  console.log("нажали на кнопку");
+};
+
+// Навесить слушатель на клик по кнопке 'О проекте'
+popupOpenButton.addEventListener("click", popupToggle);
+
+// Навесить слушатель на клик по крестику
+popupCloseButton.addEventListener("click", popupToggle);
+
+// Бонусики
+const header = document.querySelector(".header");
+header.addEventListener("click", function (event) {
+  console.log("нажали на хедер");
+  console.log(event.target);
+  console.log(event.currentTarget);
+});
+
+// Применение всплытия
+const closePopupByClickingOnOverlay = function (event) {
+  console.log(event.target);
+  console.log(event.currentTarget);
+
+  if (event.target == event.currentTarget) {
+    popupToggle();
+  }
+};
+
+popup.addEventListener("click", closePopupByClickingOnOverlay);
